67b1d2ccf37c85df5f72dd5981b9d850fc645373
heleleleoyuncu <discilatif0@gmail.com>
2026-05-24 09:32:25 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-IG_USERNAME = os.environ.get("IG_USERNAME", "uwu._.mucuk")
-IG_PASSWORD = os.environ.get("IG_PASSWORD", "")
-IG_SESSION_ID = os.environ.get("IG_SESSION_ID", "")
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "")
-BOT_OWNER = os.environ.get("BOT_OWNER", "uwu._.mucuk")
-PREFIX = "!"
-MAX_HISTORY = 10
